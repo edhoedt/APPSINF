161b8237ec2fcb253d25bba2dee76dc6dfff9eda
Siceron <ludovicfastre@yahoo.fr>
2013-05-07 15:14:10 +0200

    Delete settings

--- Asteroid/src/game/Field.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class Field {
-
-	// CONFIG
-	public static boolean FULLSCREEN = false;
-	public static final boolean VSYNC = true;
-	public static final boolean SHOWFPS = false;
-}
